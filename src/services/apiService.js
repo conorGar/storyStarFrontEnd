81771fd82e77187f39c8005a45125662e5953a83
Conor Garity <conorgarity@Conors-MacBook-Pro.local>
2019-09-17 13:40:30 -0400

    baseURL test

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -3,6 +3,8 @@ import axios from 'axios'
 const BASE_URL = process.env.REACT_APP_HEROKU_URL  //|| `http://localhost:8001/`
 const JWT_TOKEN = localStorage.getItem('token')
 
+console.log(BASE_URL)
+
 export const apiCall = axios.create({
     baseURL: BASE_URL,
     headers: {
